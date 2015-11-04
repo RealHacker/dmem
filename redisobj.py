a4cdc89ed479e2c31d660b1d4a7222f42b9598fa
RealHacker <wonglei@gmail.com>
2015-11-04 18:58:54 +0800

    finish

--- redisobj.py
+++ redisobj.py
@@ -4,7 +4,7 @@ import random, string
 import contextlib
 from utils import initialize_redis, RedisNotInitialized, RedisOperationFailure
 
-redis_client = initialize_redis()
+redis_client = initialize_redis("192.168.1.7", 6379)
 used_redis_addresses = set()
 
 def get_a_valid_redis_addr(t):
@@ -48,16 +48,93 @@ def get_value_from_object_and_type(obj, t):
     else:
         return None
 
-INDEXOF_LUA_SCRIPT = """
+FIND_ITEM_LUA_SCRIPT= """
 local key = KEYS[1]
 local obj = ARGV[1]
+local tkey = KEYS[2]
+local tobj = ARGV[2]
 local items = redis.call('lrange', key, 0, -1)
+local types = redis.call('lrange', tkey, 0, -1)
 for i = 1, #items do
-    if items[i] == obj then
+    if items[i] == obj and types[i] == tobj then
         return i - 1
     end
 end 
 return -1
+"""
+
+REVERSE_LUA_SCRIPT = """
+local key = KEYS[1]
+local tkey = KEYS[2]
+local llen = redis.call('llen', key)
+local half = math.floor(llen/2)
+for i = 1, half do
+    local obj1 = redis.call('lindex', key, i-1)
+    local obj2 = redis.call('lindex', key, llen-i)
+    redis.call('lset', key, i-1, obj2)
+    redis.call('lset', key, llen-i, obj1)
+    obj1 = redis.call('lindex', tkey, i-1)
+    obj2 = redis.call('lindex', tkey, llen-i)
+    redis.call('lset', tkey, i-1, obj2)
+    redis.call('lset', tkey, llen-i, obj1)
+end
+"""
+
+INSERT_LUA_SCRIPT = """
+local key = KEYS[1]
+local tkey = KEYS[2]
+local idx = tonumber(ARGV[1])
+local obj = ARGV[2]
+local t = ARGV[3]
+
+local llen = redis.call('llen', key)
+if idx >= llen then 
+    redis.call('rpush', key, obj)
+    redis.call('rpush', tkey, t)
+else
+    local val = redis.call('lindex', key, idx)
+    local valt = redis.call('lindex', tkey, idx)
+    redis.call('lset', key, idx, '_REDIS_LUA_PLACEHOLDER_')
+    redis.call('lset', tkey, idx, '_REDIS_LUA_PLACEHOLDER_')
+    redis.call('linsert', key, 'BEFORE', '_REDIS_LUA_PLACEHOLDER_', obj)
+    redis.call('linsert', tkey, 'BEFORE', '_REDIS_LUA_PLACEHOLDER_', t)
+    redis.call('lset', key, idx+1, val)
+    redis.call('lset', tkey, idx+1, valt)
+end
+"""
+
+COUNT_LUA_SCRIPT = """
+local key = KEYS[1]
+local tkey = KEYS[2]
+local target = ARGV[1]
+local t = ARGV[2]
+local items = redis.call('lrange', key, 0, -1)
+local types = redis.call('lrange', tkey, 0, -1)
+local cnt = 0
+for i = 1, #items do
+    if items[i] == target and types[i] == t then
+        cnt = cnt+1
+    end
+end 
+return cnt
+"""
+
+DELSLICE_LUA_SCRIPT = """
+local key = KEYS[1]
+local tkey = KEYS[2]
+local start = tonumber(ARGV[1])
+local stop = tonumber(ARGV[2])
+
+local llen = redis.call('llen', key)
+if stop > llen then
+    stop = llen
+end
+for i = start, stop-1 do
+    redis.call('lset', key, i, '_REDIS_LUA_PLACEHOLDER_')
+    redis.call('lset', tkey, i, '_REDIS_LUA_PLACEHOLDER_')
+end
+redis.call('lrem', key, stop-start, '_REDIS_LUA_PLACEHOLDER_')
+redis.call('lrem', tkey, stop-start, '_REDIS_LUA_PLACEHOLDER_')
 """
 
 DELETE_PLACE_HOLDER = "__TO_BE_DELETED__"
@@ -242,36 +319,6 @@ class RedisList(dbase):
             pipe.lset(self._type_addr_, idx, t)
             pipe.execute()
 
-    def __getslice__(self, start, end):
-        """
-        >>> l = RedisList([110, 2.0, False, "abc", RedisStr("xyz")])
-        >>> l[0:-1]
-        [110, 2.0, False, 'abc']
-        >>> l[-2:5]
-        ['abc', <__main__.RedisStr object at 0x01C06BB0>]
-        """
-        if self.cache:
-            return self.cache[start:end]
-        if end==0:
-            return []
-        objs = redis_client.lrange(self._addr_, start, end-1)
-        ts = redis_client.lrange(self._type_addr_, start, end-1)
-        values = []
-        for obj, t in zip(objs, ts):
-            values.append(get_value_from_object_and_type(obj, t))
-        return values
-
-    def __contains__(self, val):
-        if self.cache:
-            return val in self.cache
-        obj, t = get_redis_object_and_type(val)
-        idx = redis_client.eval(INDEXOF_LUA_SCRIPT, 1, self._addr_, obj)
-        if idx < 0:
-            return False
-        tt = redis_client.lindex(self._type_addr_, idx)
-        # todo: this is incorrect
-        return t == tt
-
     def __delitem__(self, idx):
         """
         >>> l = RedisList([1,2.0,True,"abc", RedisStr("xyz")])
@@ -292,6 +339,52 @@ class RedisList(dbase):
             pipe.lset(self._type_addr_, idx, DELETE_PLACE_HOLDER)
             pipe.lrem(self._type_addr_, 1, DELETE_PLACE_HOLDER)
             pipe.execute()
+
+    def __getslice__(self, start, end):
+        """
+        >>> l = RedisList([110, 2.0, False, "abc", RedisStr("xyz")])
+        >>> l[0:-1]
+        [110, 2.0, False, 'abc']
+        >>> l[-2:5]
+        ['abc', <__main__.RedisStr object at 0x01C06BB0>]
+        """
+        if self.cache:
+            return self.cache[start:end]
+        if end==0:
+            return []
+        objs = redis_client.lrange(self._addr_, start, end-1)
+        ts = redis_client.lrange(self._type_addr_, start, end-1)
+        values = []
+        for obj, t in zip(objs, ts):
+            values.append(get_value_from_object_and_type(obj, t))
+        return values
+
+    def __delslice__(self, start, end):
+        """
+        >>> l = RedisList([2.0, "sdfdsf", RedisList([1,2,3])])
+        >>> del l[1:2]
+        >>> l._load()
+        [2.0, <__main__.RedisList object at 0x7f2154a24390>]
+        >>> del l[1:12]
+        >>> l._load()
+        [2.0]
+        """
+        if self.cache:
+            del self.cache[start:end]
+        if start == 0:
+            redis_client.ltrim(self._addr_, end, -1)
+            redis_client.ltrim(self._type_addr_, end, -1)
+        else:
+            redis_client.eval(DELSLICE_LUA_SCRIPT, 2, self._addr_, self._type_addr_, start, end)
+
+    def __contains__(self, val):
+        if self.cache:
+            return val in self.cache
+        obj, t = get_redis_object_and_type(val)
+        idx = redis_client.eval(FIND_ITEM_LUA_SCRIPT, 2, self._addr_, self._type_addr_, obj, t)
+        if idx < 0:
+            return False
+        return True
 
     def append(self, val):
         """
@@ -341,18 +434,27 @@ class RedisList(dbase):
 
     def remove(self, val):
         # this is a bit convoluted, don't call this unless necessary
+        """
+        >>> l = RedisList([2.0, "sdfdsf", RedisList([1,2,3])])
+        >>> l.append("abc")
+        >>> l.append(12)
+        >>> l.remove("abc")
+        >>> l._load()
+        [2.0, 'sdfdsf', <__main__.RedisList object at 0x7f5110745190>, 12]
+        """
         if self.cache:
             self.cache.remove(val)
         obj, t = get_redis_object_and_type(val)
-        with redis_client.pipeline() as pipe:
-            pipe.eval(INDEXOF_LUA_SCRIPT, 1, self._addr_, obj)
-            pipe.lrem(self._addr_, obj, 1)
-            [idx, removed] = pipe.execute()
-        if idx >= 0 and removed:
+        idx = redis_client.eval(FIND_ITEM_LUA_SCRIPT, 2, self._addr_, self._type_addr_, obj, t)
+
+        if idx >= 0:
             with redis_client.pipeline() as pipe:
                 # First mark the type at index to be deleted, then call LREM
+                pipe.lset(self._addr_, idx, DELETE_PLACE_HOLDER)
+                pipe.lrem(self._addr_, 1, DELETE_PLACE_HOLDER)
                 pipe.lset(self._type_addr_, idx, DELETE_PLACE_HOLDER)
-                pipe.lrem(self._type_addr_, DELETE_PLACE_HOLDER, 1)
+                pipe.lrem(self._type_addr_, 1, DELETE_PLACE_HOLDER)
+                pipe.execute()
 
     def sort(self):
         """
@@ -367,19 +469,57 @@ class RedisList(dbase):
         redis_client.sort(self._addr_, store=self._addr_) # sort in place
 
     def index(self, val):
+        """
+        >>> l = RedisList([2.0, "sdfdsf", RedisList([1,2,3])])
+        >>> l.append("2.3")
+        >>> 2.3 in l
+        False
+        >>> "2.3" in l
+        True
+        >>> l.append(2.3)
+        >>> 2.3 in l
+        True
+        >>> l.index(2.3)
+        4L
+        >>> l.index("2.3")
+        3L
+        """
         if self.cache:
             return self.cache.index(val)
         obj, t = get_redis_object_and_type(val)
-        idx = redis_client.eval(INDEXOF_LUA_SCRIPT, 1, self._addr_, obj)
-        # This is incorrect
+        idx = redis_client.eval(FIND_ITEM_LUA_SCRIPT, 2, self._addr_, self._type_addr_, obj, t)
         return idx
 
     def reverse(self):
         if self.cache:
             self.cache.reverse()
-        # TODO: use lua script
+        redis_client.eval(REVERSE_LUA_SCRIPT, 2, self._addr_, self._type_addr_)
 
     def insert(self,idx, val):
-        pass
+        if self.cache:
+            self.cache.insert(idx, val)
+        obj, t = get_redis_object_and_type(val)
+        redis_client.eval(INSERT_LUA_SCRIPT, 2, self._addr_, self._type_addr_, idx, obj, t)
 
+    def count(self, val):
+        if self.cache:
+            return self.cache.count(val)
+        obj, t = get_redis_object_and_type(val)
+        return redis_client.eval(COUNT_LUA_SCRIPT, 2, self._addr_, self._type_addr_, obj, t)
 
+    # define methods for redis specific commands
+    def lpush(self, val):
+        if self.cache:
+            self.cache.insert(0, val)
+        obj, t = get_redis_object_and_type(val)
+        redis_client.lpush(self._addr_, obj)
+        redis_client.lpush(self._type_addr_, t)
+
+    def lpop(self):
+        if self.cache:
+            v = self.cache[0]
+            del self.cache[0]
+            return v
+        obj = redis_client.lpop(self._addr_)
+        t = redis_client.lpop(self._type_addr_)
+        return get_value_from_object_and_type(obj, t)
